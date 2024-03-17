7b4bcc845d0838a98b1b4f3c445eab01b830919c
Jan-Frederic <Jan-Frederic@users.noreply.github.com>
2024-03-17 20:40:02 +0000

    deploy: 0ea4f249cb376df64b81cdb7d84350f64102de1a

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { TreeDynamicExampleComponent } from './tree-dynamic-example/tree-dynamic-example.component';
-
-export const routes: Routes = [
-    { path: 'tree', component: TreeDynamicExampleComponent
- },
-];
